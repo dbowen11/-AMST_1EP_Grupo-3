8efbb1895bb63ccf5e4056b3cc822fb82144d167
David Bowen <dbowen@espol.edu.ec>
2022-07-08 15:29:52 -0500

    Commit inicial

---
+++ app/src/main/java/com/example/amst_primeraevaluacinprctica_grupo3_bowen_torres_tumbaco/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.amst_primeraevaluacinprctica_grupo3_bowen_torres_tumbaco;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
